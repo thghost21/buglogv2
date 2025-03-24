13bc00eebbdd02d9ff24f5334573e2a381ce190e
thghost21 <tylerhacker21@gmail.com>
2025-03-24 15:37:53 -0600

    Db Context

--- src/db/DbContext.js
+++ src/db/DbContext.js
@@ -1,8 +1,10 @@
 import mongoose from 'mongoose'
 import { AccountSchema } from '../models/Account.js'
+import { BugsSchema } from '../models/Bugs.js';
 
 class DbContext {
   Account = mongoose.model('Account', AccountSchema);
+  Bugs = mongoose.model('Bug', BugsSchema)
 }
 
 export const dbContext = new DbContext()
