64ae7ef4865c8137a747e95ece5c50e7b6f99844
thghost21 <tylerhacker21@gmail.com>
2025-03-24 16:27:07 -0600

    Get by Id and edit

--- src/controllers/BugsController.js
+++ src/controllers/BugsController.js
@@ -7,15 +7,49 @@ export class BugsController extends BaseController {
     super('api/bugs')
     this.router
       .get('', this.getAllBugs)
+      .get('/:bugId', this.getBugById)
       .use(Auth0Provider.getAuthorizedUserInfo)
       .post('', this.createBug)
+      .put('/:bugId', this.editBug)
   }
+
   /**
-* Creates a new value from request body and returns the value
-* @param {import("express").Request} request
-* @param {import("express").Response} response
-* @param {import("express").NextFunction} next
-*/
+   * Creates a new value from request body and returns the value
+   * @param {import("express").Request} request
+   * @param {import("express").Response} response
+   * @param {import("express").NextFunction} next
+  */
+  async getAllBugs(request, response, next) {
+    try {
+      const bugs = await bugsService.getAllBugs()
+      response.send(bugs)
+    } catch (error) {
+      next(error)
+    }
+  }
+
+  /**
+   * Creates a new value from request body and returns the value
+   * @param {import("express").Request} request
+   * @param {import("express").Response} response
+   * @param {import("express").NextFunction} next
+  */
+  async getBugById(request, response, next) {
+    try {
+      const bugId = request.params.bugId
+      const bug = await bugsService.getBugById(bugId)
+      response.send(bug)
+    } catch (error) {
+      next(error)
+    }
+  }
+
+  /**
+   * Creates a new value from request body and returns the value
+   * @param {import("express").Request} request
+   * @param {import("express").Response} response
+   * @param {import("express").NextFunction} next
+  */
   async createBug(request, response, next) {
     try {
       const bugData = request.body
@@ -27,26 +61,23 @@ export class BugsController extends BaseController {
       next(error)
     }
   }
+
   /**
-* Creates a new value from request body and returns the value
-* @param {import("express").Request} request
-* @param {import("express").Response} response
-* @param {import("express").NextFunction} next
-*/
-  async getAllBugs(request, response, next) {
+   * Creates a new value from request body and returns the value
+   * @param {import("express").Request} request
+   * @param {import("express").Response} response
+   * @param {import("express").NextFunction} next
+  */
+  async editBug(request, response, next) {
     try {
-      const bugs = await bugsService.getAllBugs()
-      response.send(bugs)
+      const bugData = request.body
+      const bugId = request.params.bugId
+      const userInfo = request.userInfo
+      const bug = await bugsService.editBug(bugId, bugData, userInfo)
+      response.send(bug)
+
     } catch (error) {
       next(error)
     }
-
   }
-
 }
-/**
-* Creates a new value from request body and returns the value
-* @param {import("express").Request} request
-* @param {import("express").Response} response
-* @param {import("express").NextFunction} next
-*/
--- src/services/BugsService.js
+++ src/services/BugsService.js
@@ -1,4 +1,5 @@
 import { dbContext } from "../db/DbContext.js"
+import { Forbidden } from "../utils/Errors.js"
 
 class BugsService {
   async getAllBugs() {
@@ -6,10 +7,28 @@ class BugsService {
     return bugs
   }
 
+  async getBugById(bugId) {
+    const bug = await dbContext.Bugs.findById(bugId)
+    await bug.populate('creator', 'name picture')
+    return bug
+  }
+
   async createBug(bugData) {
     const bug = await dbContext.Bugs.create(bugData)
     await bug.populate('creator', 'name picture')
     return bug
+  }
+
+  async editBug(bugId, bugData, userInfo) {
+    const bugToUpdate = await this.getBugById(bugId)
+    if (bugToUpdate.creatorId != userInfo.id) {
+      throw new Forbidden('you cant edit a bug you are not the creator of')
+    }
+    bugToUpdate.updateOne(bugData)
+    bugToUpdate.title = bugData.title ?? bugToUpdate.title
+    bugToUpdate.description = bugData.description ?? bugToUpdate.description
+    await bugToUpdate.save()
+    return bugToUpdate
   }
 
 }
