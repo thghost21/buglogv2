48a5e3a33bad1acd81b9a73ae40c8697249f4f0b
thghost21 <tylerhacker21@gmail.com>
2025-03-24 15:31:43 -0600

    bug Schema built

--- src/models/Bugs.js
+++ src/models/Bugs.js
@@ -1,3 +1,18 @@
 const { Schema } = require("mongoose");
 
-const BugsSchema = new Schema
+export const BugsSchema = new Schema(
+  {
+    creatorId: { type: Schema.ObjectId, required: true, ref: 'Account' },
+    title: { type: String, required: true, minLength: 10, maxLength: 50 },
+    description: { type: String, required: true, minLength: 10, maxLength: 500 },
+    priority: { type: Number, required: true, min: 1, max: 5 },
+    closed: { type: Boolean, required: true, default: false },
+    closedDate: { type: Date }
+
+
+  },
+  {
+    timestamps: true,
+    toJSON: { virtuals: true, versionKey: false }
+  }
+)
